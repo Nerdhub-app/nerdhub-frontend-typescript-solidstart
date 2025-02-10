6c900c5e54525363e34c3db37a1bf6a8590dc060
Tolojanahary <tolotor@yahoo.fr>
2025-02-10 12:52:19 +0300

    Home page

--- src/app.tsx
+++ src/app.tsx
@@ -3,13 +3,19 @@ import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
 
+import "./app.css";
+
+import { ThemeProvider } from "~/contexts/theme";
+
 export default function App() {
   return (
     <Router
       root={(props) => (
         <MetaProvider>
           <Title>Nerdhub - Your Next-Gen chat app</Title>
-          <Suspense>{props.children}</Suspense>
+          <Suspense>
+            <ThemeProvider>{props.children}</ThemeProvider>
+          </Suspense>
         </MetaProvider>
       )}
     >
---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,11 @@
+import SiteLayout from "~/layouts/SiteLayout";
+
+export default function LandingPage() {
+  return (
+    <SiteLayout>
+      <main class="bg-surface">
+        <h1>Hello World</h1>
+      </main>
+    </SiteLayout>
+  );
+}
