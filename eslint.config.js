1e70ceb7480562960a52bd16f6fa97940faaea41
Tolojanahary <tolotor@yahoo.fr>
2025-02-10 12:47:00 +0300

    Project and editor settings update

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,16 @@
 import js from "@eslint/js";
+import tseslint from "@typescript-eslint/eslint-plugin";
 import * as tsParser from "@typescript-eslint/parser";
 import jsxA11y from "eslint-plugin-jsx-a11y";
 import solid from "eslint-plugin-solid/configs/typescript";
+import globals from "globals";
 
 export default [
   js.configs.recommended,
+  {
+    files: ["**/*.{ts,tsx}"],
+    ...tseslint.configs.recommended,
+  },
   {
     files: ["**/*.{ts,tsx}"],
     ...solid,
@@ -20,6 +26,10 @@ export default [
     ...jsxA11y.flatConfigs.recommended,
     languageOptions: {
       ...jsxA11y.flatConfigs.recommended.languageOptions,
+      globals: {
+        ...globals.serviceworker,
+        ...globals.browser,
+      },
     },
   },
 ];
