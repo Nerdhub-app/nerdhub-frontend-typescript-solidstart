e936343b0a80f44107d669d240623fd6f9d35440
Tolojanahary <tolotor@yahoo.fr>
2025-02-13 14:59:48 +0300

    App / Google fonts link tags

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,11 @@
-import { MetaProvider, Title } from "@solidjs/meta";
+import { Link, MetaProvider, Title } from "@solidjs/meta";
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
-import { Suspense } from "solid-js";
+import { Show, Suspense } from "solid-js";
 
 import "./app.css";
 
+import { isDevEnv } from "./helpers/runtime.helper";
 import { ThemeProvider } from "~/contexts/theme";
 
 export default function App() {
@@ -13,6 +14,14 @@ export default function App() {
       root={(props) => (
         <MetaProvider>
           <Title>Nerdhub - Your Next-Gen chat app</Title>
+          <Show when={!isDevEnv()}>
+            <Link rel="preconnect" href="https://fonts.googleapis.com" />
+            <Link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+            <Link
+              href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
+              rel="stylesheet"
+            ></Link>
+          </Show>
           <Suspense>
             <ThemeProvider>{props.children}</ThemeProvider>
           </Suspense>
