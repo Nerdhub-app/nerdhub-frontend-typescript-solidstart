db83ceb194529849f485d3172b576342a9d2492d
Tolojanahary <tolotor@yahoo.fr>
2025-02-13 14:56:14 +0300

    Additional configs and packages

--- app.config.ts
+++ app.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from "@solidjs/start/config";
 
 import tailwindcss from "@tailwindcss/vite";
+import solidPlugin from "vite-plugin-solid";
+import solidSvg from "vite-plugin-solid-svg";
 
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()],
+    plugins: [tailwindcss(), solidSvg()],
   },
 });
