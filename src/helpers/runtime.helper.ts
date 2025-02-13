7ecc487e2a44eaf5e630bda64b31e3ef1aac4439
Tolojanahary <tolotor@yahoo.fr>
2025-02-13 14:58:38 +0300

    Helper / Runtime / Is dev env checker

--- src/helpers/runtime.helper.ts
+++ src/helpers/runtime.helper.ts
@@ -1,3 +1,7 @@
 export function isClient() {
   return typeof window !== "undefined" && typeof document !== "undefined";
 }
+
+export function isDevEnv() {
+  return !process.env.NODE_ENV || process.env.NODE_ENV === "DEVELOPMENT";
+}
