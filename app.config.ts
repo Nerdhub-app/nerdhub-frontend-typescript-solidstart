b03fc39c4e7ae6030e4ac9b12b11113c122da3b5
Tolojanahary <tolotor@yahoo.fr>
2025-02-21 11:14:06 +0300

    .env / Webservices URL

--- app.config.ts
+++ app.config.ts
@@ -5,6 +5,11 @@ import solidPlugin from "vite-plugin-solid";
 import solidSvg from "vite-plugin-solid-svg";
 
 export default defineConfig({
+  server: {
+    runtimeConfig: {
+      webservicesUrl: "http://localhost:8081/api",
+    },
+  },
   vite: {
     plugins: [tailwindcss(), solidSvg()],
   },
